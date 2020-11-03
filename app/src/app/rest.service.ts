a5b4c2f1c78783f2751eae13d64a36fe33f4587b
Marc-Philippe Huget <mphuget@gmail.com>
2020-11-03 21:29:28 +0100

    Add the code for GET API

--- app/src/app/rest.service.ts
+++ app/src/app/rest.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+  getTodos(): Observable<any> {
+
+    const apiUrl = "http://localhost:3000/api/todos";
+
+    return this.http.get(apiUrl, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+
+  }
+
+  getTodo(id:any): Observable<any> {
+
+    const apiUrl = "http://localhost:3000/api/todo/" + id;
+
+    return this.http.get(apiUrl, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+
+  }
+
 }
