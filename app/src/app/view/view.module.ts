99e52b76ba6179b999a891aea853925f0418a242
Marc-Philippe Huget <mphuget@gmail.com>
2020-11-03 22:58:17 +0100

    View todo

--- app/src/app/view/view.module.ts
+++ app/src/app/view/view.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ViewPageRoutingModule } from './view-routing.module';
-
 import { ViewPage } from './view.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ViewPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ViewPageRoutingModule
+    RouterModule.forChild(routes)
   ],
   declarations: [ViewPage]
 })
--- app/src/app/view/view.page.ts
+++ app/src/app/view/view.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { RestService } from '../rest.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-view',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view.page.scss'],
 })
 export class ViewPage implements OnInit {
+  todo : any;
+  api : RestService;
+  id : string;
 
-  constructor() { }
+  constructor(public restapi: RestService, public loadingController: LoadingController, private route: ActivatedRoute) {
 
-  ngOnInit() {
+    this.api = restapi;
+
   }
 
+  async getTodo(id:any) {
+    const loading = await this.loadingController.create({
+      message: 'Loading'
+    });
+
+    await loading.present();
+    await this.api.getTodo(this.id)
+      .subscribe(res => {
+        console.log(res);
+        this.todo = res;
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+      });
+
+  }
+
+  save() {
+
+  }
+
+  delete() {
+
+  }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params : ParamMap)=> {
+      this.id=params.get('id');
+    });
+    console.log("Current id: " + this.id);
+    this.getTodo(this.id);
+  }
 }
